f4d8c02d712e01457da7eaff30157c0eebcaeb2a
blockchainlover2019 <blockchainlover2019@gmail.com>
2023-03-13 17:52:14 +0900

    feat: complete wallet connection

---
+++ index.js
@@ -0,0 +1,33 @@
+let _provider = null;
+let connected_state = false;
+
+const getProvider = () => {
+	console.log("windows", window);
+	if ("martian" in window) {
+  	return window.martian;
+	}
+	window.open("https://www.martianwallet.xyz/", "_blank");
+	return null;
+};
+
+async function connect_wallet() {
+	_provider = getProvider();
+
+	if (!_provider) return;
+
+	// disconnect
+	if (connected_state) {
+		await _provider.disconnect();
+		connected_state = false;
+		return false;
+	}
+
+  let status = await _provider.isConnected();
+  if (!status) {
+    await _provider.connect();
+    connected_state = true;
+    return true;
+  }
+}
+
+window.jsConnectWallet = connect_wallet;
